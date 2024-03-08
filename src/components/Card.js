59c3f659c001b7be2014732a97bf9c99234a4207
DiegoCoura <diegogomes.10.9@gmail.com>
2024-03-08 00:27:39 -0300

    feat: create Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,8 @@
-import { createCustomElement } from "../helpers.js";
+import { correctDayOffset, createCustomElement } from "../helpers.js";
 
 const Card = (weatherInfos) => {
+  const forecastDays = weatherInfos.forecast.forecastday;
+
   const container = createCustomElement("div", { className: "card-container" });
   const cardHeader = createCustomElement("div", { className: "card-header" });
 
@@ -10,8 +12,13 @@ const Card = (weatherInfos) => {
   });
 
   const temperature = createCustomElement("div", {
-    className: "temperature",
-    innerText: `${weatherInfos.current.temp_c}ºC`,
+    className: "main-temperature",
+    innerText: `${Math.round(weatherInfos.current.temp_c)}ºC`,
+  });
+
+  const conditionIcon = createCustomElement("img", {
+    className: "condition-icon",
+    src: `${weatherInfos.current.condition.icon}`,
   });
 
   const condition = createCustomElement("div", {
@@ -19,22 +26,86 @@ const Card = (weatherInfos) => {
     innerText: `${weatherInfos.current.condition.text}`,
   });
 
-  const conditionIcon = createCustomElement("img", {
-    className: "condition-icon",
-    src: `${weatherInfos.current.condition.icon}`
-  })
-
   const minMaxTemp = createCustomElement("div", {
     className: "min-max-temperature",
-    innerText: ``
-  })
+    innerText: `${Math.round(forecastDays[0].day.maxtemp_c)}º/${Math.round(
+      forecastDays[0].day.maxtemp_c
+    )}º`,
+  });
+
+  const mainFooter = createCustomElement("div", {
+    className: "main-footer",
+  });
+
+  mainFooter.appendChild(conditionIcon);
+  mainFooter.appendChild(condition);
+  mainFooter.appendChild(minMaxTemp);
 
   cardHeader.appendChild(cardTitle);
   cardHeader.appendChild(temperature);
-  cardHeader.appendChild(condition);
-  cardHeader.appendChild(conditionIcon);
+  cardHeader.appendChild(mainFooter);
+
+  const weekContainer = createCustomElement("div", {
+    className: "week-container",
+  });
+
+  forecastDays.forEach((foreDay, index) => {
+    let dayOfWeek;
+
+    if (index === 0) {
+      dayOfWeek = "Today";
+    } else {
+      dayOfWeek = correctDayOffset(new Date(forecastDays[index].date));
+    }
+
+    const weekDayRow = createCustomElement("div", {
+      className: "week-container-row",
+    });
+    const weekDay = createCustomElement("div", {
+      className: "weekday",
+      innerText: `${dayOfWeek}`,
+    });
+
+    const rainContainer = createCustomElement("div", {
+      className: "foreday-rain-container",
+    });
+
+    const dropIcon = createCustomElement("i", {
+      className: "fa-solid fa-droplet",
+    });
+
+    const rainPercentage = createCustomElement("div", {
+      className: "rain-percentage",
+      innerText: `${foreDay.day.daily_chance_of_rain}%`,
+    });
+
+    rainContainer.appendChild(dropIcon);
+    rainContainer.appendChild(rainPercentage);
+
+    const dayIcon = createCustomElement("div", { className: "day-icons" });
+    const dayWeatherImg = createCustomElement("img", {
+      className: "day-weather-img",
+      src: `${foreDay.day.condition.icon}`,
+    });
+    dayIcon.appendChild(dayWeatherImg);
+
+    const dayMaxMin = createCustomElement("div", {
+      className: "foreday-max-min",
+      innerText: `${Math.round(foreDay.day.maxtemp_c)}º ${Math.round(
+        foreDay.day.mintemp_c
+      )}º`,
+    });
+
+    weekDayRow.appendChild(weekDay);
+    weekDayRow.appendChild(rainContainer);
+    weekDayRow.appendChild(dayIcon);
+    weekDayRow.appendChild(dayMaxMin);
+
+    weekContainer.appendChild(weekDayRow);
+  });
 
   container.appendChild(cardHeader);
+  container.appendChild(weekContainer);
 
   return container;
 };
